b619d2bd01231cd8e2c50af3185fa5ca4259fd2c
Digi <hunter_digital_sl@yahoo.com>
2017-06-01 23:16:02 +0300

    Updated
    - Changed to non-entity update (to prevent issues in v1.181).
    - Minor code tweaks.

--- Data/Scripts/RealisticThrusters/Log.cs
+++ Data/Scripts/RealisticThrusters/Log.cs
@@ -1,24 +1,105 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using System.Text;
-using System.Threading.Tasks;
 using Sandbox.ModAPI;
-using Sandbox.Common;
+using VRage.Game;
+using VRage.Game.ModAPI;
 using VRage.Utils;
 
-namespace Digi.Utils
+namespace Digi
 {
-    class Log
+    public static class Log // v1.4
     {
-        public const string MOD_NAME = "Realistic Thrusters";
-        public const int WORKSHOP_ID = 575893643;
+        public static string modName = "UNNAMED";
+        public static string modFolder = "UNNAMED";
+        public static ulong workshopId = 0;
         public const string LOG_FILE = "info.log";
 
-        private static System.IO.TextWriter writer = null;
+        private static TextWriter writer = null;
         private static IMyHudNotification notify = null;
+        private static readonly StringBuilder cache = new StringBuilder(64);
+        private static readonly List<string> preInitMessages = new List<string>(0);
         private static int indent = 0;
-        private static StringBuilder cache = new StringBuilder();
+
+        public static void SetUp(string modName, ulong workshopId, string modFolder = null)
+        {
+            Log.modName = modName;
+            Log.modFolder = (modFolder == null ? modName : modFolder);
+            Log.workshopId = workshopId;
+        }
+
+        public static void Init()
+        {
+            if(MyAPIGateway.Utilities == null)
+            {
+                MyLog.Default.WriteLineAndConsole(modName + " Log.Init() called before API was ready!");
+                return;
+            }
+
+            if(writer != null)
+                Close();
+
+            writer = MyAPIGateway.Utilities.WriteFileInLocalStorage(LOG_FILE, typeof(Log));
+
+            if(preInitMessages.Count > 0)
+            {
+                foreach(var msg in preInitMessages)
+                {
+                    Log.Error(msg);
+                }
+
+                preInitMessages.Clear();
+            }
+
+            Info("Initialized");
+
+            cache.Clear();
+            cache.Append("DS=").Append(MyAPIGateway.Utilities.IsDedicated);
+            cache.Append("; defined=");
+
+#if STABLE
+            cache.Append("STABLE, ");
+#endif
+
+#if UNOFFICIAL
+            cache.Append("UNOFFICIAL, ");
+#endif
+
+#if DEBUG
+            cache.Append("DEBUG, ");
+#endif
+
+#if BRANCH_STABLE
+            cache.Append("BRANCH_STABLE, ");
+#endif
+
+#if BRANCH_DEVELOP
+            cache.Append("BRANCH_DEVELOP, ");
+#endif
+
+#if BRANCH_UNKNOWN
+            cache.Append("BRANCH_UNKNOWN, ");
+#endif
+
+            Info(cache.ToString());
+            cache.Clear();
+        }
+
+        public static void Close()
+        {
+            if(writer != null)
+            {
+                Log.Info("Unloaded.");
+
+                writer.Flush();
+                writer.Close();
+                writer = null;
+            }
+
+            indent = 0;
+            cache.Clear();
+        }
 
         public static void IncreaseIndent()
         {
@@ -27,7 +108,7 @@ namespace Digi.Utils
 
         public static void DecreaseIndent()
         {
-            if (indent > 0)
+            if(indent > 0)
                 indent--;
         }
 
@@ -41,34 +122,46 @@ namespace Digi.Utils
             Error(e.ToString());
         }
 
+        public static void Error(Exception e, string printText)
+        {
+            Error(e.ToString(), printText);
+        }
+
         public static void Error(string msg)
         {
+            Error(msg, modName + " error - open %AppData%/SpaceEngineers/Storage/" + workshopId + "_" + modFolder + "/" + LOG_FILE + " for details");
+        }
+
+        public static void Error(string msg, string printText)
+        {
             Info("ERROR: " + msg);
-            
+
             try
             {
-                MyLog.Default.WriteLineAndConsole(MOD_NAME + " error: " + msg);
-                
-                string text = MOD_NAME + " error - open %AppData%/SpaceEngineers/Storage/" + WORKSHOP_ID + "_" + MOD_NAME + "/" + LOG_FILE + " for details";
-                
-                if(notify == null)
+                MyLog.Default.WriteLineAndConsole(modName + " error/exception: " + msg);
+
+                if(MyAPIGateway.Session != null)
                 {
-                    notify = MyAPIGateway.Utilities.CreateNotification(text, 10000, MyFontEnum.Red);
+                    if(notify == null)
+                    {
+                        notify = MyAPIGateway.Utilities.CreateNotification(printText, 10000, MyFontEnum.Red);
+                    }
+                    else
+                    {
+                        notify.Text = printText;
+                        notify.ResetAliveTime();
+                    }
+
+                    notify.Show();
                 }
-                else
-                {
-                    notify.Text = text;
-                    notify.ResetAliveTime();
-                }
-                
-                notify.Show();
             }
-            catch (Exception e)
+            catch(Exception e)
             {
-                Info("ERROR: Could not send notification to local client: " + e.ToString());
+                Info("ERROR: Could not send notification to local client: " + e);
+                MyLog.Default.WriteLineAndConsole(modName + " error/exception: Could not send notification to local client: " + e);
             }
         }
-        
+
         public static void Info(string msg)
         {
             Write(msg);
@@ -78,16 +171,11 @@ namespace Digi.Utils
         {
             try
             {
-                if(writer == null)
-                {
-                    if (MyAPIGateway.Utilities == null)
-                        throw new Exception("API not initialied but got a log message: " + msg);
-
-                    writer = MyAPIGateway.Utilities.WriteFileInLocalStorage(LOG_FILE, typeof(Log));
-                }
-
                 cache.Clear();
                 cache.Append(DateTime.Now.ToString("[HH:mm:ss] "));
+
+                if(writer == null)
+                    cache.Append("(PRE-INIT) ");
 
                 for(int i = 0; i < indent; i++)
                 {
@@ -96,28 +184,22 @@ namespace Digi.Utils
 
                 cache.Append(msg);
 
-                writer.WriteLine(cache);
-                writer.Flush();
+                if(writer == null)
+                {
+                    preInitMessages.Add(cache.ToString());
+                }
+                else
+                {
+                    writer.WriteLine(cache);
+                    writer.Flush();
+                }
 
                 cache.Clear();
             }
             catch(Exception e)
             {
-                MyLog.Default.WriteLineAndConsole(MOD_NAME + " had an error while logging message='"+msg+"'\nLogger error: " + e.Message + "\n" + e.StackTrace);
+                MyLog.Default.WriteLineAndConsole(modName + " had an error while logging message='" + msg + "'\nLogger error: " + e.Message + "\n" + e.StackTrace);
             }
-        }
-        
-        public static void Close()
-        {
-            if(writer != null)
-            {
-                writer.Flush();
-                writer.Close();
-                writer = null;
-            }
-
-            indent = 0;
-            cache.Clear();
         }
     }
 }
--- Data/Scripts/RealisticThrusters/RealisticThrusters.cs
+++ Data/Scripts/RealisticThrusters/RealisticThrusters.cs
@@ -1,106 +1,84 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
-using System.IO;
-using Sandbox.Common;
-using Sandbox.Common.Components;
 using Sandbox.Common.ObjectBuilders;
-using Sandbox.Common.ObjectBuilders.VRageData;
-using Sandbox.Common.ObjectBuilders.Definitions;
-using Sandbox.Definitions;
-using Sandbox.Engine;
-using Sandbox.Engine.Physics;
-using Sandbox.Engine.Multiplayer;
-using Sandbox.Game;
 using Sandbox.Game.Entities;
-using Sandbox.Game.EntityComponents;
-using Sandbox.Game.Gui;
 using Sandbox.ModAPI;
-using Sandbox.ModAPI.Interfaces;
-using VRageMath;
-using VRage;
+using VRage.Game.Components;
 using VRage.ModAPI;
 using VRage.ObjectBuilders;
-using VRage.Game.Components;
-using VRage.Utils;
-using Digi.Utils;
-using Ingame = Sandbox.ModAPI.Ingame;
 
 namespace Digi.RealisticThrusters
 {
     [MySessionComponentDescriptor(MyUpdateOrder.AfterSimulation)]
-    public class RealisticThrusters : MySessionComponentBase
+    public class RealisticThrustersMod : MySessionComponentBase
     {
-        public bool init { get; private set; }
-        
+        private bool init = false;
+
+        public override void LoadData()
+        {
+            Log.SetUp("Realistic Thrusters", 575893643, "RealisticThrusters");
+        }
+
         public void Init()
         {
-            Log.Info("Initialized");
+            Log.Init();
             init = true;
+
+            // stop updating
+            MyAPIGateway.Utilities.InvokeOnGameThread(() => SetUpdateOrder(MyUpdateOrder.NoUpdate));
         }
-        
+
         protected override void UnloadData()
         {
             init = false;
-            
-            Log.Info("Mod unloaded");
             Log.Close();
         }
-        
-        public override void UpdateAfterSimulation()
-        {
-            if(!init)
-            {
-                if(MyAPIGateway.Session == null)
-                    return;
-                
-                Init();
-            }
-        }
-    }
-    
-    [MyEntityComponentDescriptor(typeof(MyObjectBuilder_Thrust))]
-    public class Thruster : MyGameLogicComponent
-    {
-        public override void Init(MyObjectBuilder_EntityBase objectBuilder)
-        {
-            Entity.NeedsUpdate |= MyEntityUpdateEnum.EACH_FRAME;
-        }
-        
+
         public override void UpdateAfterSimulation()
         {
             try
             {
-                var thruster = Entity as MyThrust;
-                
-                if(!thruster.IsWorking)
-                    return;
-                
-                var grid = thruster.CubeGrid;
-                
-                if(grid.Physics == null || !grid.Physics.Enabled || grid.Physics.IsStatic)
-                    return;
-                
-                var force = thruster.WorldMatrix.Forward * thruster.BlockDefinition.ForceMagnitude * thruster.CurrentStrength;
-                grid.Physics.AddForce(MyPhysicsForceType.APPLY_WORLD_FORCE, force, grid.Physics.CenterOfMassWorld, null); // cancel the thruster's force at center of mass
-                grid.Physics.AddForce(MyPhysicsForceType.APPLY_WORLD_FORCE, -force, thruster.WorldMatrix.Translation, null); // apply the thruster's force at its position
+                if(!init)
+                {
+                    if(MyAPIGateway.Session == null)
+                        return;
+
+                    Init();
+                }
             }
             catch(Exception e)
             {
                 Log.Error(e);
             }
         }
-        
-        public override void Close()
+    }
+
+    [MyEntityComponentDescriptor(typeof(MyObjectBuilder_Thrust), useEntityUpdate: false)]
+    public class Thruster : MyGameLogicComponent
+    {
+        public override void Init(MyObjectBuilder_EntityBase objectBuilder)
         {
+            NeedsUpdate = MyEntityUpdateEnum.EACH_FRAME;
         }
-        
-        public override MyObjectBuilder_EntityBase GetObjectBuilder(bool copy = false)
+
+        public override void UpdateAfterSimulation()
         {
-            return Entity.GetObjectBuilder(copy);
+            try
+            {
+                var thruster = (MyThrust)Entity;
+                var grid = thruster.CubeGrid;
+
+                if(grid.Physics == null || !grid.Physics.Enabled || grid.Physics.IsStatic || !thruster.IsWorking)
+                    return;
+
+                var thrustMatrix = thruster.WorldMatrix;
+                var force = thrustMatrix.Backward * thruster.BlockDefinition.ForceMagnitude * thruster.CurrentStrength;
+                grid.Physics.AddForce(MyPhysicsForceType.APPLY_WORLD_FORCE, -force, grid.Physics.CenterOfMassWorld, null); // cancel the thruster's force at center of mass
+                grid.Physics.AddForce(MyPhysicsForceType.APPLY_WORLD_FORCE, force, thrustMatrix.Translation, null); // apply the thruster's force at its position
+            }
+            catch(Exception e)
+            {
+                Log.Error(e);
+            }
         }
     }
 }
