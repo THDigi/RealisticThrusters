5a1a6db691a5673bb502a6cd4af7191c39f18ced
Digi <hunter_digital_sl@yahoo.com>
2019-03-08 00:16:29 +0200

    Fixed inaccurate thrust
    - Fixed inaccurate removal of original thrust by adding force at grid-group CoM instead of grid CoM.
    - Fixed thrust being applied 1 update frame behind.
    - Updated project and removed some unnecessary things.

---
+++ Data/Scripts/RealisticThrusters/Thruster.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Sandbox.Common.ObjectBuilders;
+using Sandbox.Game.Entities;
+using Sandbox.ModAPI;
+using VRage.Game;
+using VRage.Game.Components;
+using VRage.ModAPI;
+using VRage.ObjectBuilders;
+using VRage.Utils;
+
+namespace Digi.RealisticThrusters
+{
+    [MyEntityComponentDescriptor(typeof(MyObjectBuilder_Thrust), useEntityUpdate: false)]
+    public class Thruster : MyGameLogicComponent
+    {
+        private MyThrust thruster;
+
+        public override void Init(MyObjectBuilder_EntityBase objectBuilder)
+        {
+            thruster = (MyThrust)Entity;
+            NeedsUpdate = MyEntityUpdateEnum.EACH_FRAME;
+        }
+
+        public override void UpdateBeforeSimulation()
+        {
+            try
+            {
+                var grid = thruster.CubeGrid;
+
+                if(grid.Physics == null || !grid.Physics.Enabled || grid.Physics.IsStatic || !thruster.IsWorking)
+                    return;
+
+                var thrustMatrix = thruster.WorldMatrix;
+                var force = thrustMatrix.Backward * thruster.BlockDefinition.ForceMagnitude * thruster.CurrentStrength;
+                var groupProperties = MyGridPhysicalGroupData.GetGroupSharedProperties(grid);
+
+                // cancel the thruster's force at grid-group center of mass
+                grid.Physics.AddForce(MyPhysicsForceType.APPLY_WORLD_FORCE, -force, groupProperties.CoMWorld, null);
+
+                // apply the thruster's force at its position
+                grid.Physics.AddForce(MyPhysicsForceType.APPLY_WORLD_FORCE, force, thrustMatrix.Translation, null);
+            }
+            catch(Exception e)
+            {
+                MyLog.Default.WriteLineAndConsole($"{e.Message}\n{e.StackTrace}");
+
+                if(MyAPIGateway.Session?.Player != null)
+                    MyAPIGateway.Utilities.ShowNotification($"[ ERROR: {GetType().FullName}: {e.Message} | Send SpaceEngineers.Log to mod author ]", 10000, MyFontEnum.Red);
+            }
+        }
+    }
+}
